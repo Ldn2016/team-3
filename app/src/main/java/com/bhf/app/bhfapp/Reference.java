99284382f53b16f16126156cb8f6b7798604003b
albylillington <albylillington@gmail.com>
2016-12-10 00:35:19 +0000

    Commit test

---
+++ app/src/main/java/com/bhf/app/bhfapp/Reference.java
@@ -0,0 +1,14 @@
+package com.bhf.app.bhfapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reference);
+        System.out.println("Opened page!");
+    }
+}
