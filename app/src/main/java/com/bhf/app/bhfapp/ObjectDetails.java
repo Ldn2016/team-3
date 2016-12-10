d78366a528c840ace42442718c5ff2fd3876e104
harveyives <harveyives@gmail.com>
2016-12-10 04:12:16 +0000

    object details now work woohoo

--- app/src/main/java/com/bhf/app/bhfapp/ObjectDetails.java
+++ app/src/main/java/com/bhf/app/bhfapp/ObjectDetails.java
@@ -7,7 +7,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -32,33 +36,26 @@ public class ObjectDetails extends AppCompatActivity {
 //                item = datum;
 //            }
 //        }
+
         AddItem item = data.get(0);
 
-        TextView ref = new TextView(this);
-        ref.setTextSize(40);
-        ref.setText(item.getRefNumber());
 
-        TextView objectName = new TextView(this);
-        ref.setTextSize(40);
-        ref.setText(item.getObject());
+        ArrayList<String> attributeList = new ArrayList<>();
+        attributeList.add("Rerefence Number: "+item.getRefNumber());
+        attributeList.add("Name: "+item.getObject());
+        attributeList.add("Status: "+((item.getStatus()) ? "Sold":"Pending"));
+        attributeList.add("Points: "+Integer.toString(item.getPoints()));
+        attributeList.add("Date: "+item.getCurrentDate().toString());
 
-        TextView status = new TextView(this);
-        ref.setTextSize(35);
 
-        if (item.getStatus()) {
-            ref.setText("Sold");
-        }
-        else {
-            ref.setText("Pending");
-        }
 
-        TextView points = new TextView(this);
-        ref.setTextSize(35);
-        ref.setText(Integer.toString(item.getPoints()));
 
-        TextView date = new TextView(this);
-        ref.setTextSize(35);
-        ref.setText(item.getCurrentDate().toString());
+
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_listview, attributeList);
+
+        ListView listView = (ListView) findViewById(R.id.item_list_object_details);
+        listView.setAdapter(adapter);
     }
 
 }
