1484caa79b8b846904f8cc8812f813ad18570884
harveyives <harveyives@gmail.com>
2016-12-10 03:48:50 +0000

    changed objectdetails

--- app/src/main/java/com/bhf/app/bhfapp/ObjectDetails.java
+++ app/src/main/java/com/bhf/app/bhfapp/ObjectDetails.java
@@ -9,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ObjectDetails extends AppCompatActivity {
 
     @Override
@@ -19,7 +22,17 @@ public class ObjectDetails extends AppCompatActivity {
         Intent intent = getIntent();
 
         String refKey = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        AddItem item = MainDatabase.checkRef(refKey); //
+
+//
+        InternalDatabase internalDatabase = new InternalDatabase();
+        ArrayList<AddItem> data = internalDatabase.getDatabase();
+//        AddItem item = null;
+//        for(AddItem datum:data){
+//            if(datum.getRefNumber().equals(refKey)){
+//                item = datum;
+//            }
+//        }
+        AddItem item = data.get(0);
 
         TextView ref = new TextView(this);
         ref.setTextSize(40);
@@ -41,11 +54,11 @@ public class ObjectDetails extends AppCompatActivity {
 
         TextView points = new TextView(this);
         ref.setTextSize(35);
-        ref.setText(item.getPoints());
+        ref.setText(Integer.toString(item.getPoints()));
 
         TextView date = new TextView(this);
         ref.setTextSize(35);
-        ref.setText((CharSequence) item.getCurrentDate());
+        ref.setText(item.getCurrentDate().toString());
     }
 
 }
