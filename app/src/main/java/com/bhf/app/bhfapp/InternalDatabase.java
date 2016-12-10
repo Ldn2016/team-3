f3beb038a35fb14a718e905a75c201e113389c9d
Alfie Douglas <alfiejdouglas@gmail.com>
2016-12-10 01:39:55 +0000

    new internal database

--- app/src/main/java/com/bhf/app/bhfapp/InternalDatabase.java
+++ app/src/main/java/com/bhf/app/bhfapp/InternalDatabase.java
@@ -1,9 +1,9 @@
 public class InternalDatabase{
 	MainDatabase d = new MainDatabase();
 	ArrayList<AddItem> database = new ArrayList<AddItem>();
-	public static void main(String[] args){
-	    addToList("ABCDEFGHIJ");
-	}
+	//public static void main(String[] args){
+	  //  addToList("ABCDEFGHIJ");
+	//}
 	
 	public void addToList(String refNumber){
 		try{
