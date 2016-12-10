b5c55bf852942e19bfc1db2818bc28b9f4c34982
albylillington <albylillington@gmail.com>
2016-12-10 00:42:19 +0000

    added a comment for harvey to see

--- app/src/main/java/com/bhf/app/bhfapp/MainDatabase.java
+++ app/src/main/java/com/bhf/app/bhfapp/MainDatabase.java
@@ -1,7 +1,12 @@
+import com.bhf.app.bhfapp.AddItem;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class MainDatabase{
 	ArrayList<AddItem> list = new ArrayList<AddItem>();
 	Date currentDate = new Date();
-	
+
 	public void addItem(String refNumber, String object, boolean status, int points, Date currentDate){
 		list.add(AddItem(refNumber, object, status, points, currentDate));
 	}
