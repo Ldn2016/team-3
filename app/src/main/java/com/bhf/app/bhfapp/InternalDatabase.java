4a3fe54da6225b93eaf10b14948f16a3ca00cabd
Alfie Douglas <alfiejdouglas@gmail.com>
2016-12-10 01:57:06 +0000

    fixed merge on internaldatabase

--- app/src/main/java/com/bhf/app/bhfapp/InternalDatabase.java
+++ app/src/main/java/com/bhf/app/bhfapp/InternalDatabase.java
@@ -3,16 +3,12 @@ import java.util.*;
 public class InternalDatabase{
 	MainDatabase d = new MainDatabase();
 	ArrayList<AddItem> database = new ArrayList<AddItem>();
-<<<<<<< HEAD
-	//public static void main(String[] args){
-	  //  addToList("ABCDEFGHIJ");
-	//}
-=======
+
 	static InternalDatabase internal = new InternalDatabase();
 	/*public static void main(String[] args){
 		internal.addToList("ABCDEFGHIJ");
 	}*/
->>>>>>> af7f07dacaaf295fa4b5909c02e55223f6568321
+
 	
 	public void addToList(String refNumber){
 		if(d.checkRef(refNumber)==null){
