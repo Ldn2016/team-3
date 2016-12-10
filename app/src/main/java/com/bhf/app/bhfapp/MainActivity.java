646334d2567b51788a1d432dde04873a4e31661e
harveyives <harveyives@gmail.com>
2016-12-10 01:21:40 +0000

    added toast for when list item is clicked

--- app/src/main/java/com/bhf/app/bhfapp/MainActivity.java
+++ app/src/main/java/com/bhf/app/bhfapp/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String[] donateList = {"hello","world","harvey"};
@@ -20,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.item_list);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Toast.makeText(getBaseContext(),"Test",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     protected void openReferencePage(View view){
