f7e93c75ed3464461ac68d4c5d9af7df2e77ee79
Hafeez <har4@st-andrews.ac.uk>
2016-12-10 00:01:13 +0000

    object details activity set textViews

---
+++ app/src/main/java/com/bhf/app/bhfapp/ObjectDetails.java
@@ -0,0 +1,44 @@
+package com.bhf.app.bhfapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ObjectDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_object_details);
+
+        Intent intent = getIntent();
+
+        AddItem item = (AddItem) intent.getParcelableExtra("");
+
+        TextView ref = new TextView(this);
+        ref.setTextSize(40);
+        ref.setText(item.getRefNumber());
+
+        TextView objectName = new TextView(this);
+        ref.setTextSize(40);
+        ref.setText(item.getObject());
+
+        TextView status = new TextView(this);
+        ref.setTextSize(35);
+        ref.setText(item.getStatus());
+
+        TextView points = new TextView(this);
+        ref.setTextSize(35);
+        ref.setText(item.getPoints());
+
+        TextView date = new TextView(this);
+        ref.setTextSize(35);
+        ref.setText(item.getDate());
+    }
+
+}
